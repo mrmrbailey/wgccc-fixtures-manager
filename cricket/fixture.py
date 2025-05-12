41376dcca3748ac21caf252c1c493c298f20973f
Mark Bailey <mr.m.r.bailey@gmail.com>
2025-05-12 16:45:04 +0100

    Update fixture.py
    broke summary into home and away

--- cricket/fixture.py
+++ cricket/fixture.py
@@ -1,20 +1,24 @@
 from datetime import datetime, timezone
 
 class Fixture:
-    def __init__(self, summary, fixture_date, fixture_time, ground):
-        self.summary = summary
+    def __init__(self, home, away, division, fixture_date, fixture_time, ground):
+        self.home = home
+        self.away = away
+        self.division = division
         self.fixture_date = fixture_date
         self.fixture_time = fixture_time
         self.ground = ground
 
     def __eq__(self, other):
-        return (self.summary == other.summary
+        return (self.home == other.home
+                and self.away == other.away
+                and self.division == other.division
                 and self.fixture_date == other.fixture_date
                 and self.fixture_time == other.fixture_time
                 and self.ground == other.ground)
 
     def __str__(self):
-        return f"{self.summary} {self.fixture_date} {self.fixture_time} {self.ground}"
+        return f"{get_summary(self)} {self.fixture_date} {self.fixture_time} {self.ground}"
 
     def __lt__(self, other):
         self_date_split_string = self.fixture_date.split("/")
@@ -32,16 +36,20 @@ class Fixture:
         return self_fixture_date_time < other_fixture_date_time
 
 class InvalidFixture:
-    def __init__(self, fixture, type, source):
+    def __init__(self, fixture, invalid_type, source):
         self.fixture = fixture
-        self.type = type
+        self.invalid_type = invalid_type
         self.source = source
 
     def __lt__(self, other):
-        return self.fixture.summary < other.fixture.summary
+        return self.fixture.home < other.fixture.home
 
     def __str__(self):
         return f"{self.fixture} {self.source}"
+
+def get_summary(fixture):
+    summary = fixture.home + ' vs ' + fixture.away
+    return summary
 
 def print_fixtures_list(list_of_fixtures):
     list_of_fixtures.sort()
