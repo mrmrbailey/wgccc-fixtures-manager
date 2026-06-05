3d920b6bf360ddb3e40591391ae623a06cb6b1fb
Mark Bailey <mr.m.r.bailey@gmail.com>
2026-06-05 12:53:29 +0100

    cleaned up googlecalendar

--- cricket/reader/googlecalendar.py
+++ cricket/reader/googlecalendar.py
@@ -1,4 +1,6 @@
 # imports
+from typing import List
+
 from fixture import Fixture
 from fixture_enums import Ground, Location, FixtureType
 from reader.utils import get_google_calendar_path
@@ -6,57 +8,52 @@ from reader.googlecalendar_utils import clean_summary, get_teams, get_fixture_ty
     get_fixture_type_from_summary, clean_fixture_date, is_fixture_this_year, is_postponed
 from cricket_team import CricketTeam
 
-from icalendar import Calendar
+from icalendar import Calendar, Component
 from os import listdir
 
+def parse_record(ground: Ground, event: Component) -> Fixture:
+    summary = event['SUMMARY']
+    fixture_start_date = clean_fixture_date(event.get("DTSTART").dt)
+    fixture_end_date = clean_fixture_date(event.get("DTEND").dt)
+    teams = get_teams(clean_summary(summary))
 
-def parse_ical(filename, ground):
-    fixtures = []
+    fixture_type = FixtureType.POSTPONED if is_postponed(summary) else get_fixture_type_from_description(event.get("Description"))
+    if fixture_type is None:
+        fixture_type = get_fixture_type_from_summary(summary)
+    if ground == Ground.AWAY:
+        wgc_team = CricketTeam.get_value(teams[1])
+        oppo = teams[0]
+        location = Location.AWAY
+    else:
+        wgc_team = CricketTeam.get_value(teams[0])
+        oppo = teams[1]
+        location = Location.HOME
 
-    file = open(filename, 'rb')
-    cal = Calendar.from_ical(file.read())
+    return Fixture(wgc_team,
+                      oppo,
+                      location,
+                      fixture_type,
+                      fixture_start_date,
+                      fixture_end_date,
+                      ground)
 
-    for event in cal.walk('vevent'):
-        summary = event['SUMMARY']
-        fixture_start_date = clean_fixture_date(event.get("DTSTART").dt)
-
-        if is_fixture_this_year(fixture_start_date):
-            fixture_end_date = clean_fixture_date(event.get("DTEND").dt)
-            teams = get_teams(clean_summary(summary))
-
-            fixture_type = FixtureType.POSTPONED if is_postponed(summary) else get_fixture_type_from_description(event.get("Description"))
-            if fixture_type is None:
-                fixture_type = get_fixture_type_from_summary(summary)
-            if ground == Ground.AWAY:
-                wgc_team = CricketTeam.get_value(teams[1])
-                oppo = teams[0]
-                location = Location.AWAY
-            else:
-                wgc_team = CricketTeam.get_value(teams[0])
-                oppo = teams[1]
-                location = Location.HOME
-
-            fixture = Fixture(wgc_team,
-                              oppo,
-                              location,
-                              fixture_type,
-                              fixture_start_date,
-                              fixture_end_date,
-                              ground)
-            fixtures.append(fixture)
-    file.close()
-    return fixtures
-
-def parse_google_calendar_data():
+def parse_google_calendar_data() -> List[Fixture]:
     fixtures = []
     for filename in listdir(get_google_calendar_path()):
         if filename.endswith('.ics'):
-            if filename.startswith(Ground.DP.value):
-                ground = Ground.DP
-            elif filename.startswith(Ground.WPF.value):
-                ground = Ground.WPF
-            else:
-                ground = Ground.AWAY
-            fixtures.extend(parse_ical(get_google_calendar_path() + filename, ground))
+            file = open(get_google_calendar_path() + filename, 'rb')
+            cal = Calendar.from_ical(file.read())
 
+            for event in cal.walk('vevent'):
+                if is_fixture_this_year(clean_fixture_date(event.get("DTSTART").dt)):
+                    fixtures.append(parse_record(get_ground(filename), event))
+            file.close()
     return fixtures
+
+def get_ground(filename:str) -> Ground:
+    if filename.startswith(Ground.DP.value):
+        return Ground.DP
+    elif filename.startswith(Ground.WPF.value):
+        return Ground.WPF
+    else:
+        return Ground.AWAY
