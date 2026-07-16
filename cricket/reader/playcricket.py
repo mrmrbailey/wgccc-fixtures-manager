12949d88ef09fa25cde7423dc9a98a2611a918be
Mark Bailey <mr.m.r.bailey@gmail.com>
2026-07-16 17:14:45 +0100

    removed duplicate call

--- cricket/reader/playcricket.py
+++ cricket/reader/playcricket.py
@@ -66,5 +66,5 @@ def parse_all_play_cricket_data(check_result: bool):
                 for record in list(csv_reader)[1:]:
                     fixture = parse_record(record)
                     if not check_result or is_fixture_missing_result(fixture, record[14]):
-                        fixtures.append(parse_record(record))
+                        fixtures.append(record)
     return fixtures
